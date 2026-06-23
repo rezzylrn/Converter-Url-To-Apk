00155a7c3349223a31ba7bade2b523b7ceb30ed9
Rezzlrna <rezzstck@gmail.com>
2026-06-23 17:48:39 +0700

    Update download message for Java SDK

--- jdk.js
+++ jdk.js
@@ -1,7 +1,7 @@
 const { execSync } = require('child_process');
 
 try {
-    console.log("Mulai download Java 17 Portable (Sabar bontot, ukurannya sekitar 180MB)...");
+    console.log("starting download java sdk, wait a minuetes...");
     execSync('curl -L -o java.tar.gz https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz', { stdio: 'inherit' });
     
     console.log("Success, Extracking file...");
